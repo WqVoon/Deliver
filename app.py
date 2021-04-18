8a19d6ff462054e18be38095acd3166c25463f63
WqVoon <2027195958@qq.com>
2021-04-18 11:29:44 +0800

    为 app 实例注册了 401，404，500 错误码

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
-from config import app_config
 
 def create_app():
 	app = Flask(__name__)
+
+	# 注册配置信息
+	from config import app_config
 	app.config.from_mapping(app_config)
 	
+	# 注册错误码处理函数
+	from code_handlers import (
+		handle_401, handle_404, handle_500
+	)
+	app.register_error_handler(401, handle_401)
+	app.register_error_handler(404, handle_404)
+	app.register_error_handler(500, handle_500)
+
+	# 注册测试蓝图，url前缀 /test
 	from test import test_bp
 	app.register_blueprint(test_bp)
 
---
+++ code_handlers.py
@@ -0,0 +1,10 @@
+def handle_401(e):
+	return "Who are u?"
+
+
+def handle_404(e):
+	return "No such page"
+
+
+def handle_500(e):
+	return str(e)
