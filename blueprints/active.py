80a2028f74d49ea8051fd6b61666aecaf25f426d
myhh <liyuman0707@outlook.com>
2021-05-08 20:10:40 +0800

    实现了 /active/query/i-add 用来查询已被接取的我的订单 实现了 /active/query/i-fetch 用来查询我接取的订单

--- blueprints/active.py
+++ blueprints/active.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, abort
 from flask_login import current_user, login_required
+from ..model import Active
+from ..utils import get_one_page_orders
 
 
 active_bp = Blueprint("active", __name__, url_prefix="/active")
@@ -8,3 +10,21 @@ active_bp = Blueprint("active", __name__, url_prefix="/active")
 @active_bp.route("/")
 def active_index():
 	return "Hello, Active!"
+
+
+@active_bp.route("/query/i-add/<int:page_id>")
+@login_required
+def active_query_i_add(page_id):
+	"""查询我下过的所有订单，返回值同 /pending/query/all"""
+	query = Active.query.filter_by(buyer_id=current_user.id)
+	items = get_one_page_orders(query, page_id)
+	return { "items": items }
+
+
+@active_bp.route("/query/i-fetch/<int:page_id>")
+@login_required
+def active_query_i_fetch(page_id):
+	"""查询我接过的所有订单，返回值同 /pending/query/all"""
+	query = Active.query.filter_by(receiver_id=current_user.id)
+	items = get_one_page_orders(query, page_id)
+	return { "items": items }
